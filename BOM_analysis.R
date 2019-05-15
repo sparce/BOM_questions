9439f5163bfffd07957ff7bd58b4101e11cfaa90
Stephen <36123293+sparce@users.noreply.github.com>
2019-05-15 11:15:49 +1000

    Q2a answer and restructure answer to Q1

--- BOM_analysis.R
+++ BOM_analysis.R
@@ -10,9 +10,12 @@ BOM_stations <- read_csv("data/BOM_stations.csv")
 #    Temp min/max are stored in the same column, will need to separate it, then filter for rows
 #    that are not '-' in the three measurements, then summarise the number of rows left per station
 
-q1_ans <- BOM_data %>% 
+#Will need this data frame again later
+BOM_with_temps <- BOM_data %>% 
   separate(Temp_min_max, into = c("t_min", "t_max"), sep = "/") %>% 
-  filter(t_min != "-", t_max != "-", Rainfall != "-") %>% 
+  filter(t_min != "-", t_max != "-", Rainfall != "-")
+
+q1_ans <- BOM_with_temps %>% 
   group_by(Station_number) %>% 
   summarise(num_records = n())
 
@@ -21,3 +24,28 @@ q1_ans
 
 #Save it to a file 
 write_csv(q1_ans, "results/q1_record_completeness_by_station.csv")
+
+#Q2: Which month saw the lowest difference between minimum and maximum temperatures in a day? 
+#    And which state saw the highest?
+#
+#    Month data is found in BOM_data, state is in BOM_stations so will need a join for the last part
+#    To find the *difference* between min amd max temp, will need to mutate the data. Can then
+#    arrange it to look at highest/lowest.
+
+BOM_with_temps %>% 
+  mutate(t_diff = as.numeric(t_max) - as.numeric(t_min)) %>%  # t_min/t_max are text ("<chr>") need to convert them to numbers 
+  arrange(t_diff)
+
+# Quite a few days with equal min and max temps. 
+# Let's look at the differences averaged across a month instead
+q2a_ans <- BOM_with_temps %>% 
+  mutate(t_diff = as.numeric(t_max) - as.numeric(t_min)) %>%
+  group_by(Month) %>% 
+  summarise(avg_t_diff = mean(t_diff)) %>% 
+  arrange(avg_t_diff)
+
+#Print it to the screen if running in RStudio
+#Can see that it's Month 6 (June) with the lowest average temp difference 
+q2a_ans
+
+
